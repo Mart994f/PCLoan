a921df55823a05e205e20eaf37449fbcf2b38cd6
Martin Rosenquist (mart994f) <mart994f@zbc.dk>
2020-09-10 14:05:58 +0200

    Made some models for handling the computer data

---
+++ PCLoan/Models/ComputerRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PCLoan.Models
+{
+    public class ComputerRepository
+    {
+        public List<Computer> GetAllComputers()
+        {
+            
+        }
+    }
+}
