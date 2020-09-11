d45344d0959bccb312cfb0b272ccca42374ca90a
Martin Rosenquist (mart994f) <mart994f@zbc.dk>
2020-09-11 12:56:54 +0200

    Working on Admin site

--- PCLoan/Controllers/AdminController.cs
+++ PCLoan/Controllers/AdminController.cs
@@ -1,8 +1,7 @@
-﻿using PCLoan.Models;
-using System;
+﻿using PCLoan.Data;
+using PCLoan.Models;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace PCLoan.Controllers
@@ -12,11 +11,18 @@ namespace PCLoan.Controllers
         // GET: Admin
         public ActionResult Index()
         {
-            
+            IEnumerable<SelectListItem> states = DbDataAccess.GetData<StateModel>("GetStates", null)
+                .Select(s => new SelectListItem
+                {
+                    Value = s.Id.ToString(),
+                    Text = s.State
+                });
 
-            return View();
+            List<ComputerModel> computers = DbDataAccess.GetData<ComputerModel>("GetAllComputers", null).ToList();
+            computers.ForEach(c => c.states = states);
+            computers.Where(c => c.states. = c.SelectedState)
+
+            return View(computers);
         }
-
-
     }
 }
---
+++ PCLoan/Models/ComputerModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace PCLoan.Models
+{
+    public class ComputerModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [Display(Name = "Status")]
+        public string SelectedState { get; set; }
+        public IEnumerable<SelectListItem> states { get; set; }
+
+        [Display(Name = "Udlånt til:")]
+        public string LendBy { get; set; }
+
+        [Display(Name = "Lånt:")]
+        public DateTime LoanDate { get; set; }
+
+        [Display(Name = "Afleværes:")]
+        public DateTime ReturnDate { get; set; }
+    }
+}
