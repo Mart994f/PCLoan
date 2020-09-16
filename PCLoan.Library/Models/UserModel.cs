82a79a660aaf0cb9ffcd8d209b05d1f69c467e1f
Martin Rosenquist <mart994f@zbc.dk>
2020-09-17 00:52:52 +0200

    Added Authentication logic to the PCLoan.Library

--- PCLoan.Library/Models/UserModel.cs
+++ PCLoan.Library/Models/UserModel.cs
@@ -1,11 +1,50 @@
-﻿namespace PCLoan.Library.Models
+﻿using PCLoan.Library.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.DirectoryServices.AccountManagement;
+using System.Security;
+
+namespace PCLoan.Library.Models
 {
-    class UserModel
+    /// <summary>
+    /// A model representing a user.
+    /// </summary>
+    [Table("User")]
+    public class UserModel
     {
+        /// <summary>
+        /// The users ID.
+        /// </summary>
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        /// <summary>
+        /// The users username.
+        /// </summary>
+        [Required(AllowEmptyStrings = false)]
+        public string UserName { get; set; }
 
-        public string Password { get; set; }
+        /// <summary>
+        /// The users password.
+        /// </summary>
+        [NotMapped]
+        public SecureString Password { get; set; }
+
+        /// <summary>
+        /// If the user is authenticated.
+        /// </summary>
+        [NotMapped]
+        public bool Authenticated { get; set; } = false;
+
+        /// <summary>
+        /// The role of the user.
+        /// </summary>
+        [NotMapped]
+        public Role? Role { get; set; } = null;
+
+        /// <summary>
+        /// The <see cref="UserPrincipal"/> of the user.
+        /// </summary>
+        [NotMapped]
+        public UserPrincipal UserPrincipal { get; set; } = null;
     }
 }
