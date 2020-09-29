368e0c7cb65e87fb90c6d7e788b1fbffd5307f03
Martin Rosenquist <mrquist_94@hotmail.com>
2020-09-29 13:59:19 +0200

    Added ignored files

---
+++ PCLoan.Data.Library/Repositorys/Log/LogRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using PCLoan.Data.Library.Models;
+
+namespace PCLoan.Data.Library.Repositorys
+{
+    public class LogRepository : Repository<LogModelDAO>, ILogRepository
+    {
+        #region Private Fields
+
+        #endregion
+
+        #region Public Properties
+
+        #endregion
+
+        #region Constructors
+
+        public LogRepository(IConfiguration configuration) : base(configuration)
+        {
+
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+
+        #region Private Helper Methods
+
+        #endregion
+    }
+}
